3e6be2b6372696bfc8eeb3624634213592ca2a56
Rahul Hembrom <69256240+zammyk@users.noreply.github.com>
2023-03-03 14:56:12 +0530

    Add operate function

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ function multiply(num1,num2) {
 function divide(num1,num2) {
     return num1/num2;
 }
+
+function operate(operator,num1,num2){
+    let result;
+    switch(operator){
+        case '+':
+            result = add(num1,num2);
+            break;
+        case '-':
+            result = subtract(num1,num2);
+            break;
+        case '*':
+            result = multiply(num1,num2);
+            break;
+        case '/':
+            result = divide(num1,num2);
+            break;
+        default:
+            result = undefined;
+    }
+    return result;
+}
