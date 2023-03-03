c23ffa8a921b332fc50bf2d633a2d21ec298023d
Rahul Hembrom <69256240+zammyk@users.noreply.github.com>
2023-03-03 15:48:54 +0530

    Create function to display numbers pressed

--- script.js
+++ script.js
@@ -34,3 +34,25 @@ function operate(operator,num1,num2){
     }
     return result;
 }
+
+function updateDisplay()
+{
+    display.textContent = currNum;
+}
+
+function pressNum(e){
+    const pressedNum = this.id.substring(5);
+    currNum+=pressedNum;
+    updateDisplay();
+}
+
+let currNum = "";
+
+const display = document.getElementById('display');
+const digitButtons = Array.from(document.querySelectorAll('button')).filter(button => {
+    return button.id.substring(0,5) == 'digit';
+});
+
+digitButtons.forEach(digitButton => {
+    digitButton.addEventListener('click',pressNum);
+});
